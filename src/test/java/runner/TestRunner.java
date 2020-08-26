100b0c55a2a5d1b92704e839e831b61ceec1fd18
Admin <Admin@DESKTOP-RTNAA7K>
2020-08-26 13:33:54 +0530

    cucumber classes

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,17 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "./src/test/resources/Login.feature",//used to provide path of the feature file
+glue = {"stepDefnitions"}//used to provide package name where are steps are implemented
+
+		)
+//System.out.println("hi");
+public class TestRunner {
+
+}
